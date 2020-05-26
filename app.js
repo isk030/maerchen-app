87087f58ffcf094a0f1307056acabe864751caed
Iskender Dilaver <idi@gecko.de>
2020-05-26 13:23:50 +0200

    mongoose init

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 import axios from 'axios';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://isk030:iskender123@cluster0-xfnrc.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("connected");
+});
+
